3b68df87ac71e2ace477e5581dd76890e2745493
Yousha Aleayoub <Yousha.A@Hotmail.com>
2025-12-23 12:25:13 +0330

    Update rector.php

--- rector.php
+++ rector.php
@@ -17,8 +17,11 @@ return RectorConfig::configure()
     ])
     ->withRootFiles()
     ->withIndent(' ', 4)
-    ->withPhpVersion(PhpVersion::PHP_74)
     ->withTypeCoverageLevel(10)
     ->withDeadCodeLevel(10)
     ->withCodeQualityLevel(10)
-    ->withCodingStyleLevel(10);
+    ->withCodingStyleLevel(10)
+    // Force Rector to use PHP x.x features only.
+    ->withPhpVersion(PhpVersion::PHP_74)
+    // Import Rector sets relevant for PHP x.x.
+    ->withSets([SetList::PHP_74]);
