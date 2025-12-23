48862835fc1eb670a0fded3a7fc2b4e1cb5b0e72
Yousha Aleayoub <Yousha.A@Hotmail.com>
2025-12-23 12:24:21 +0330

    Update rector.php

--- rector.php
+++ rector.php
@@ -9,18 +9,21 @@ return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/.',
     ])
+    // Skip non-project directories.
     ->withSkip([
         __DIR__ . '/.git',
         __DIR__ . '/.github',
-        __DIR__ . '/resources',
         __DIR__ . '/vendor',
+        __DIR__ . '/resources',
     ])
     ->withRootFiles()
     ->withIndent(' ', 4)
-    ->withPhpVersion(PhpVersion::PHP_85)
     ->withComposerBased(phpunit: true)
-    ->withPhpSets(php85: true)
     ->withTypeCoverageLevel(10)
     ->withDeadCodeLevel(10)
     ->withCodeQualityLevel(10)
-    ->withCodingStyleLevel(10);
+    ->withCodingStyleLevel(10)
+    // Force Rector to use PHP x.x features only.
+    ->withPhpVersion(PhpVersion::PHP_85)
+    // Import Rector sets relevant for PHP x.x.
+    ->withPhpSets(php85: true);
